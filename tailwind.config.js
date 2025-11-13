4f6699040d71a840190fc1fceb47c091ed1fb6f9
tiplergarry-commits <tiplergarry@gmail.com>
2025-11-12 22:00:44 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,17 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
-        beige: "#f5f5dc",
+        beige: "#f5f5dc"
       },
       fontFamily: {
-        barlowCondensed: ['"Barlow Condensed"', "sans-serif"],
-      },
-    },
+        barlowCondensed: ['"Barlow Condensed"', "sans-serif"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
